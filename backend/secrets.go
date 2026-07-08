56b87d48ebaf0872f096b38da7b97984127f0825
mikeruiz-sec <miguel.ruiz@smallpdf.com>
2026-07-08 19:43:07 +0200

    adding a new parameter

--- backend/secrets.go
+++ backend/secrets.go
@@ -60,3 +60,4 @@ v/Ow5T0q5gIJAiEAyS4RaI9YG8EWx/2w0T67ZUVAw8eOMB6BIUg0Xcu+3okCIBOs
 /5OiPgoTdSy7bcF9IGpSE8ZgGKzgYQVZeN97YE00
 -----END RSA PRIVATE KEY-----`
 const demoToken = "ghp_0123456789abcdefghijklmnopqrstuvwxyz"
+const demoToken = "ghp_0123456789abcdefghijklmnopqrstuvwxyz"
